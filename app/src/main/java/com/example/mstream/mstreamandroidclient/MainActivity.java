13ddede66018aa6fdee0278ecb2077c27be9ae28
Sergey <sergeypanichevpm@gmail.com>
2017-11-13 13:18:46 +0700

    Create mStreamApiTasks and Login page
    Add permissions; add material design; add log in page and styles; add login page; add Globals; add mStreamApiTasks (нужен для работы с API, для примера внутри есть класс GetWebToken, по аналогии с ним нужно создавать другие классы для использования API, сам класс ApiRequest не изменяется);

--- app/src/main/java/com/example/mstream/mstreamandroidclient/MainActivity.java
+++ app/src/main/java/com/example/mstream/mstreamandroidclient/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.mstream.mstreamandroidclient;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+interface AsyncResponse {
+    void getJWTResponse(JSONObject response);
+}
+
+public class MainActivity extends AppCompatActivity implements  AsyncResponse {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+        final EditText usernameText = (EditText) findViewById(R.id.usernameText);
+        final EditText passwordText = (EditText) findViewById(R.id.passwordText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
+            @Override
+            public void onClick(View v) {
+                GetWebToken getJWTTask = new GetWebToken();
+                getJWTTask.delegate = MainActivity.this;
+                JSONObject data = new JSONObject();
+                JSONObject obj = new JSONObject();
+                try {
+                    data.put("username", usernameText.getText().toString());
+                    data.put("password", passwordText.getText().toString());
+                    obj.put("data", data);
+                    obj.put("addr", "/login");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                getJWTTask.execute(obj);
+            }
+        });
+    }
+
+    @Override
+    public void getJWTResponse(JSONObject response) {
+        try {
+            Globals.jwt = response.getString("token");
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage(Globals.jwt)
+                    .setTitle("JWT");
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        } catch (Exception e) {
+        }
     }
 }
---
+++ app/src/main/java/com/example/mstream/mstreamandroidclient/mStreamApiTasks.java
@@ -0,0 +1,59 @@
+package com.example.mstream.mstreamandroidclient;
+
+import android.os.AsyncTask;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+class ApiRequest {
+    JSONObject doPostRequest(JSONObject... params) {
+        JSONObject result = null;
+        try {
+            String address = Globals.address + params[0].getString("addr");
+            URL object = new URL(address);
+            HttpURLConnection con = (HttpURLConnection) object.openConnection();
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            con.setRequestMethod("POST");
+            JSONObject cred = params[0].getJSONObject("data");
+            OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+            wr.write(cred.toString());
+            wr.flush();
+            StringBuilder sb = new StringBuilder();
+            int HttpResult = con.getResponseCode();
+            if (HttpResult == HttpURLConnection.HTTP_OK) {
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader(con.getInputStream(), "utf-8"));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                br.close();
+                result = new JSONObject(sb.toString());
+            } else {
+                System.out.println(con.getResponseMessage());
+            }
+        } catch (Exception ignored) {
+        }
+        return result;
+    }
+}
+
+class GetWebToken extends AsyncTask<JSONObject, Integer, JSONObject> {
+    AsyncResponse delegate;
+    private ApiRequest apiRequest = new ApiRequest();
+
+    @Override
+    protected JSONObject doInBackground(JSONObject... params) {
+        return apiRequest.doPostRequest(params);
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject response) {
+        delegate.getJWTResponse(response);
+    }
+}
